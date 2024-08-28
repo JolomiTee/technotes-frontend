9cd3fa72bd862e71b69cc16b45c4b3519063524e
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-08-28 23:47:54 +0100

    - added new package

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
-
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider store={store}>
